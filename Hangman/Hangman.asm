9e2494608f80f3b532831d82e69d315d3a2903ca
judebattista <jbattista20@my.whitworth.edu>
2017-11-21 13:30:30 -0800

    Add MASM template for the final project

---
+++ Hangman/Hangman.asm
@@ -0,0 +1,17 @@
+TITLE Hangman	(hangman.asm)
+; Description:    A two-player ASCII art hangman game
+; Authors: The Fighting Rutabagas featuring
+		Tersa Motbaynor Almaw
+		Andrew McNeill
+		Jude Battista
+
+INCLUDE Irvine32.inc    
+
+.data
+
+.code
+main proc
+
+	invoke ExitProcess,0
+main endp
+end main
